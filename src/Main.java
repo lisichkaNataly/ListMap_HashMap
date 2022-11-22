0bc7142143b1659f75d8b7dc1b67d4d0eb6ecfe4
Izmaylova <Natalyatf@yandex.ru>
2022-11-22 16:41:32 +0500

    Создание Map<String, List<Integer>> + преобразование в новую коллекцию Домашнее задание 2 Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ListMap listMap = new ListMap();
+
+        //Начальная коллекция
+        System.out.println(listMap.keysAndValues());
+
+        System.out.println();
+        // Преобразованная коллекция
+        System.out.println( listMap.convertCollection());
+
+
+
     }
 }
